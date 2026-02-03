97864ffa5355a7d594719d8d32efe8cc9a473193
Christian Castro <criscasche@hotmail.com>
2026-02-03 16:55:49 -0500

    Update version.js

--- assets/js/version.js
+++ assets/js/version.js
@@ -7,7 +7,7 @@
  Descripción   : Control centralizado de versión de la app
 ========================================================= */
 
-const APP_VERSION = "V 2.0 · Compilación 1.68";
+const APP_VERSION = "V 2.0 · Compilación 1.69";
 
 /* Inyecta versión en todos los elementos .app-version */
 document.addEventListener("DOMContentLoaded", () => {
