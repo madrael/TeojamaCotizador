b7b7416839ee7b7c62a76444dd9be9eefd47b50b
Christian Castro <criscasche@hotmail.com>
2026-04-18 10:09:25 -0500

    Update finance.js

--- assets/js/finance.js
+++ assets/js/finance.js
@@ -2,7 +2,7 @@
  Proyecto      : Cotizador de Vehículos Teojama
  Archivo       : finance.js
  Versión       : V 2.0
- Compilación   : 1.45
+ Compilación   : 1.46
  Estado        : AJUSTE MODELO FINANCIERO (SEGURO FINANCIADO)
  Descripción   :
    - Seguro anual se financia
